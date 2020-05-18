b204abbab2a8d00c3b47e783546f7a6e3027416a
John Daugherty <jdaugherty0713@gmail.com>
2020-05-18 06:22:56 -0400

    split vote count into up and down votes

--- StableGeniusExtension/ext/src/inject/app.js
+++ StableGeniusExtension/ext/src/inject/app.js
@@ -16,21 +16,23 @@ $(function ()
 		//debugger;
 
 		// figure out what the previous and next states were, then calculate a vote magnitude to add to the vote count
-		var voteMagnitude = 0;
+		var voteMagnitudeUp = 0;
+		var voteMagnitudeDn = 0;
 		var unDid = false;
 		if (voteButtonClicked == "up") {
 			switch (voteButtonData) {
 				case undefined:
 					//console.log("detected upvote of user: ", author);
-					voteMagnitude = 1;
+					voteMagnitudeUp = 1;
 					break;
 				case "down":
 					//console.log("detected switch-downvote of user: ", author);
-					voteMagnitude = 2;
+					voteMagnitudeUp = 1;
+					voteMagnitudeDn = -1;
 					break;
 				case "up":
 					//console.log("detected un-upvote of user: ", author);
-					voteMagnitude = -1;
+					voteMagnitudeUp = -1;
 					unDid = true;
 					break;
 			}
@@ -38,15 +40,16 @@ $(function ()
 			switch (voteButtonData) {
 				case undefined:
 					//console.log("detected downvote of user: ", author);
-					voteMagnitude = -1;
+					voteMagnitudeDn = 1;
 					break;
 				case "up":
 					//console.log("detected switch-upvote of user: ", author);
-					voteMagnitude = -2;
+					voteMagnitudeUp = -1;
+					voteMagnitudeDn = 1;
 					break;
 				case "down":
 					//console.log("detected un-downvote of user: ", author);
-					voteMagnitude = 1;
+					voteMagnitudeDn = -1;
 					unDid = true;
 					break;
 			}
@@ -56,21 +59,26 @@ $(function ()
 		chrome.storage.sync.get(["users"], function (result) {
 			//debugger;
 
-			var voteValue = 0;
+			var voteValueUp = 0;
+			var voteValueDn = 0;
 
 			// if a vote count exists, grab it
-			if (result.users !== undefined && result.users[author] !== undefined && result.users[author].voteValue !== undefined) {
+			if (result.users !== undefined && result.users[author] !== undefined && (result.users[author].voteValueUp !== undefined || result.users[author].voteValueDn !== undefined)) {
 				//debugger;
 
-				voteValue = result.users[author].voteValue;
+				voteValueUp = result.users[author].voteValueUp;
+				voteValueDn = result.users[author].voteValueDn;
 			} else if (unDid) {
-				voteValue = -voteMagnitude; // user has no previous vote value in the sync storage and we just detected an undo so there must be some sort of disconnect happening, so just trust the vote element and do an undo
+				// user has no previous vote value in the sync storage and we just detected an undo so there must be some sort of disconnect happening, so just trust the vote element and do an undo
+				voteValueUp = -voteMagnitudeUp;
+				voteValueDn = -voteMagnitudeDn;
             }
 
 			//debugger;
 
 			// update the vote count
-			voteValue += voteMagnitude;
+			voteValueUp += voteMagnitudeUp;
+			voteValueDn += voteMagnitudeDn;
 	      		
 	  		// if there is no information for this author, initialize new
 			var authorDetails = result || {};
@@ -78,21 +86,29 @@ $(function ()
 			authorDetails.users[author] = authorDetails.users[author] || {};
 
 	  		// save the vote count
-			authorDetails.users[author].voteValue = voteValue;
+			authorDetails.users[author].voteValueUp = voteValueUp;
+			authorDetails.users[author].voteValueDn = voteValueDn;
 
+			var finalVoteValue = voteValueUp - voteValueDn;
 	      	// write the vote count back out to sync storage
 			chrome.storage.sync.set(authorDetails, function() {
 				// update the front end on success
 				var voteValueElement = $(".vote_value_" + author);
 
-				if (voteValue == 0)
+				if (finalVoteValue == 0)
 					voteValueElement.html("");
-				else
-					voteValueElement.html("[" + voteValue + "]");
+				else {
+					voteValueElement.html("[" + finalVoteValue + "]");
+
+					//debugger;
+
+					$(".vote_value_" + author).css("background-color", "rgba(" + (finalVoteValue < 0 ? "192, 0" : "0, 192") + ", 0, " + (Math.abs(finalVoteValue) / 10) + ")");
+				}
 	        });		
 		});
 	});
 
+	// place inline elements and update user information
 	var url = chrome.runtime.getURL('/src/inject/sg_inline_content.html');
 	$.ajax({
 		url: url,
@@ -123,8 +139,10 @@ $(function ()
 							if (result.users[author] !== undefined) {
 
 								// update vote count
-								if (result.users[author].voteValue != undefined && result.users[author].voteValue != 0) {
-									$(".vote_value_" + author).html("[" + result.users[author].voteValue + "]");
+								var finalVoteValue = (result.users[author].voteValueUp || 0) - (result.users[author].voteValueDn || 0);
+								if (finalVoteValue != 0) {
+									$(".vote_value_" + author).html("[" + finalVoteValue + "]");
+									$(".vote_value_" + author).css("background-color", "rgba(" + (finalVoteValue < 0 ? "192, 0" : "0, 192") + ", 0, " + (Math.abs(finalVoteValue) / 10) + ")");
 								}
 
 								// update tag
@@ -140,79 +158,9 @@ $(function ()
         }
 	});
 
-	/*
-	// run through each user and add an author vote count element, and load up vote counts if they exist (shows nothing if no vote count or vote count = 0)
-	$(".author").replaceWith(function (index, origText) {
-		var authorElement = $(this);
-		var author = authorElement.html();
-		var origHtml = authorElement[0].outerHTML;
-
-		//debugger;
-
-		// async get the author data and update the vote count if it exists
-		chrome.storage.sync.get(["users"], function (result) {
-			if (result.users !== undefined && result.users[author] !== undefined) {
-
-				// update vote count
-				if (result.users[author].voteValue != undefined && result.users[author].voteValue != 0) {
-					$(".vote_value_" + author).html("[" + result.users[author].voteValue + "]");
-				}
-
-				// update tag
-				if (result.users[author].authorTag) {
-					$(".author_tag_" + author).html("[" + result.users[author].authorTag + "]");
-					$(".author_tag_" + author).attr("title", result.users[author].authorTag);
-				}
-			}
-		});
-
-		// add the vote count element
-		if (author != "deleted") {
-			origHtml = "<span class='sg_insert_element'><div class='sg_inline_content'>"
-				+ origHtml
-				+ "&nbsp;<div class='vote_value_"
-				+ author
-				+ "'></div>"
-				+ "&nbsp;<div class='sg_author_tag author_tag_"
-				+ author
-				+ " icon-whhg-keyboarddelete' title='No tag for this user'></div>"
-				+ "</div></span>";
-		}
-
-		return origHtml;
-	});
-	*/
-	/*
-	// run through each user and add an author tag element, and load up author tags if they exist
-	$(".details").append(function (index, origText) {
-		var authorElement = $(this).find(".author");
-		var author = authorElement.html();
-		var origHtml = "";
-
-		//debugger;
-
-		var authorTag = undefined;
-
-		// async get the author data and replace the blank tag if it exists
-		chrome.storage.sync.get(["users"], function (result) {
-			if (result.users !== undefined && result.users[author] !== undefined && result.users[author].authorTag) {
-				$(".author_tag_" + author).html("[" + result.users[author].authorTag + "]");
-				$(".author_tag_" + author).attr("title", result.users[author].authorTag);
-			}
-
-		});
-
-		// add the author tag element
-		if (author != "deleted") {
-			origHtml = "<span class='sg_insert_element'><div class='sg_inline_content'>&nbsp;<div class='sg_author_tag author_tag_" + author + " icon-whhg-keyboarddelete' title='No tag for this user'></div></div></span>";
-		}
-
-		return origHtml;
-	});
-	*/
-
 	//debugger;
 
+	// sets up the functionality for clicking the user tag
 	setAuthorTagClick();
 
 	console.log(">>> end of DOM ready");
